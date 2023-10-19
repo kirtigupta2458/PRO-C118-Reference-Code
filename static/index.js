fb72f1b5043575a1db90ce1ce3131b62dd44c854
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-10-19 21:27:31 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -13,23 +13,7 @@ $(function () {
             "text": $("#text").val()
         }
         $.ajax({
-            type: 'POST',
-            url: "/predict-emotion",
-            data: JSON.stringify(input_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function (result) {
-                $("#prediction").html(result.data.predicted_emotion)
-                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
-                $('#prediction').css("display", "");
-                $('#emo_img_url').css("display", "");
-                predicted_emotion = result.data.predicted_emotion
-                $('#save_button').prop('disabled', false);
-            },
-            error: function (result) {
-                alert(result.responseJSON.message)
-            }
-        });
+           
     });
     $("#save_button").click(function () {
         save_data = {
@@ -37,20 +21,7 @@ $(function () {
             "text": $("#text").val(),
             "emotion": predicted_emotion
         }
-        $.ajax({
-            type: 'POST',
-            url: "/save-entry",
-            data: JSON.stringify(save_data),
-            dataType: "json",
-            contentType: 'application/json',
-            success: function () {
-                alert("Your entry has been saved successfully!")
-                window.location.reload()
-            },
-            error: function (result) {
-                alert(result.responseJSON.message)
-            }
-        });
+        
 
     });
 })
